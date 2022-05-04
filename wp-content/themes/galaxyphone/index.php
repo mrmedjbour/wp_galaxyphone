e681d1ab4b5c5da3ec7e6a8dca075ef535e9fc1a
MEDJBOUR MOHAMMED <me@medjbour.me>
2022-05-05 00:09:29 +0100

    add header and footer func to index

--- wp-content/themes/galaxyphone/index.php
+++ wp-content/themes/galaxyphone/index.php
@@ -1,71 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-
-<head>
-    <meta charset="utf-8">
-    <title>Bootstrap to WordPress 2.0</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <?php wp_head(); ?>
-
-</head>
-
-<body class="blog">
-
-<div id="top-navigation">
-    <div class="container">
-        <div class="row justify-content-end">
-            <div class="col-md-6">
-                <nav class="main-menu">
-                    <ul class="top-menu d-flex flex-row navigation top-menu justify-content-end list-unstyled">
-                        <li class="menu-item"><a href="index.html">Home</a></li>
-
-                        <li class="menu-item menu-item-has-children"><a href="index.html">Blog</a>
-
-                            <ul class="sub-menu">
-                                <li class="menu-item"><a href="single.html">Single post</a></li>
-                                <li class="menu-item"><a href="widgets.html">Widgets</a></li>
-                                <li class="menu-item"><a href="index.html">Contact</a></li>
-
-                                <li class="menu-item menu-item-has-children"><a href="index.html">Blog</a>
-                                    <ul class="sub-menu">
-                                        <li class="menu-item"><a href="index.html">Blog</a></li>
-                                        <li class="menu-item"><a href="index.html">FAQ</a></li>
-                                        <li class="menu-item"><a href="index.html">Contact</a></li>
-                                    </ul>
-                                </li>
-
-                            </ul>
-
-                        </li>
-
-                        <li class="menu-item"><a href="widgets.html">Widgets</a></li>
-                        <li class="menu-item"><a href="index.html">Contact</a></li>
-                        <li class="menu-item special-menu"><a href="index.html">Join</a></li>
-                    </ul>
-                </nav>
-
-                <button type="button" class="navbar-open">
-                    <i class="mobile-nav-toggler flaticon flaticon-menu"></i>
-                </button>
-
-            </div>
-        </div>
-
-        <!-- Mobile Menu -->
-        <div class="mobile-menu">
-            <div class="menu-backdrop"></div>
-            <div class="close-btn">
-                <i class="flaticon flaticon-close"></i>
-            </div>
-            <nav class="menu-box">
-                <ul class="navigation clearfix"></ul>
-            </nav>
-        </div>
-
-    </div>
-</div>
+<?php
+    get_header();
+?>
 
 <section class="title-banner">
     <div class="container">
@@ -154,25 +89,6 @@
     </div>
 </div>
 
-<footer>
-    <div class="footer-calltoaction text-center">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 offset-md-2 overflow-hidden">
-                    <p class="sub-title">Join the course</p>
-                    <h2>Bootstrap to WordPress 2.0</h2>
-                    <p>Learn how to design and build custom, beautiful & responsive WordPress websites and themes for beginners in 2021 and beyond!</p>
-                    <a href="#" class="btn btn-primary">Join now -&gt;</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="copyright text-center">
-        <p>&copy; Copyright Brightside Studios Inc.</p>
-    </div>
-</footer>
-
-<?php wp_footer(); ?>
-</body>
-
-</html>
+<?php
+    get_footer();
+?>
